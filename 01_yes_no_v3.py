2a1495c1c4feb97e2aa122945b4a2617bb6e26a5
diggersofcode <masterbaconhair@gmail.com>
2022-04-05 09:10:19 +1200

    YEs/no

--- 01_yes_no_v3.py
+++ 01_yes_no_v3.py
@@ -5,24 +5,26 @@ for testing
 
 """
 
+show_instructions = ""
+while show_instructions != "x":
 
-#Ask the user if they have played it before
-show_instructions = input("Have you played this game before?: ")
+    #Ask the user if they have played it before
+    show_instructions = input("Have you played this game before?: ")
 
-#IF they say yes output "Program Continues"
-if show_instructions == "yes" or show_instructions == "y":
-    print("program continues")
-
-
-#If they say no output "Display Instructions"
-elif show_instructions == "no" or show_instructions == "n":
-    print("Display instructions")
+    #IF they say yes output "Program Continues"
+    if show_instructions == "yes" or show_instructions == "y":
+        print("program continues")
 
 
-# Otherwise "show error"
-else:
-    print("Please answer 'yes or 'no'")
+    #If they say no output "Display Instructions"
+    elif show_instructions == "no" or show_instructions == "n":
+        print("Display instructions")
 
-print(f"You entered '{show_instructions}' ")
+
+    # Otherwise "show error"
+    else:
+        print("Please answer 'yes or 'no'")
+
+    print(f"You entered '{show_instructions}' ")
 
 
