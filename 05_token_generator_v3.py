d00236c58032e9e81466e2a00565b341d9724ed7
diggersofcode <masterbaconhair@gmail.com>
2022-04-12 09:29:00 +1200

    Catchup

---
+++ 05_token_generator_v3.py
@@ -0,0 +1,32 @@
+"""Component 3 (random tokens) v3
+Format currency
+Ensure odds favour the house - %10 chance of unicorn and 30% chance for each of donkey,
+zebra, or horse"""
+
+import random
+
+
+tokens = ["unicorn",
+          "horse", "horse", "horse",
+          "donkey", "donkey", "donkey",
+           "zebra", "zebra", "zebra"]
+STARTING_BALANCE = 100
+balance = STARTING_BALANCE
+# Testing loop to generate 100 tokens
+for item in range(500):
+    token = random.choice(tokens)
+    print(token, end='\t') # can wrap output making it easier to screenshot
+
+    # adjust balance
+    if token == "unicorn":
+        balance += 4
+    elif token == "donkey":
+        balance -= -1
+    else:
+        balance -= .50
+
+        # output
+print(f"Starting balance = ${STARTING_BALANCE:.2f}")
+print(f"Final balance = ${balance:.2f}")
+
+
